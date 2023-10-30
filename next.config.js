02aa651ad618d72bbcccc2a291927bb4545799bf
Mohamed Zahran <mohamed.zahran1@live.com>
2023-10-30 08:10:22 +0200

    added swagger docs, modified next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Transpile Swagger UI React https://github.com/swagger-api/swagger-ui/issues/8245
+    transpilePackages: [
+      "react-syntax-highlighter",
+      "swagger-client",
+      "swagger-ui-react",
+    ],
+    experimental: {
+      serverComponentsExternalPackages: ['swagger-jsdoc'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
